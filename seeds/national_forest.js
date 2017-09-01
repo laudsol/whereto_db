903649fce326b1ec111c02e3d27cade6dde40ad8
Sol Laudon <blaudon1@gmail.com>
2017-08-31 23:07:08 -0600

    finished adding national forest

--- seeds/national_forest.js
+++ seeds/national_forest.js
@@ -1,13 +1,14 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('national_forests').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('national_forests').insert([
+        {id: 1, name: 'White River National Forest'},
+        {id: 2, name: 'Roosevelt National Park'}
       ]);
+    })
+    .then(function(){
+    return knex.raw("SELECT setval('national_forests_id_seq', (SELECT MAX(id) FROM national_forests))");
     });
 };
