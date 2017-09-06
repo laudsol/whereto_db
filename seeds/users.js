60460739c0308d3fe761afe3d70570415362e83e
Sol Laudon <blaudon1@gmail.com>
2017-09-06 13:03:35 -0600

    added value to user seed file

--- seeds/users.js
+++ seeds/users.js
@@ -3,7 +3,7 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, fb_id:''}
+        {id: 1, fb_id:'someid'}
       ]);
     })
     .then(function(){
