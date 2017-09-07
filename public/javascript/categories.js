f7a91fa03e4c4e2e845522e60c1ebf26245af9d1
Sol Laudon <blaudon1@gmail.com>
2017-09-07 09:27:05 -0600

    all award box scrollable

--- public/javascript/categories.js
+++ public/javascript/categories.js
@@ -1,20 +1,15 @@
 $(document).ready(()=>{
-
   $.ajax({
     type: "GET",
     url: '/everyaward'
     })
     .done((data)=>{
-      console.log(data);
       data.forEach((el)=>{
         let awardText = $('<div>').addClass('awardText').text(el.type)
         let awardContainer = $('<div>').addClass('award')
         awardContainer.append(awardText)
         $('.awardBoxAll').append(awardContainer)
       });
-
   }).fail((err)=>{
-    console.log(err);
   })
-
 })
